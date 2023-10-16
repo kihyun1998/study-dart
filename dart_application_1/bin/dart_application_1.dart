67e174d532e9531f82cf27f8537e9546cb3c52e6
wantSome <kihyun1998@naver.com>
2023-10-16 20:54:03 +0900

    var

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,5 +1,17 @@
 import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1.calculate()}!');
+void main() {
+
+  // [정리] 1. var 
+  // 묵시적
+  // 함수나 메소드 내부에서 지역 변수를 선언할 때 사용
+  var implied = "묵시적";
+  implied = "implied";
+
+  //명시적
+  // class에서 변수나 속성을 선언할 때 사용
+  String explicit = "명시적";
+  explicit = "explicit";
+
+  // 그래서 지금과 같은 상황에서는 var를 사용하는걸 권장한다.
 }
